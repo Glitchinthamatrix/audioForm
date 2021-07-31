5629d8b2de914d1182cedb65acf8b19b20fc72cd
Nitesh <ny040021@gmail.com>
2021-07-31 14:52:38 +0530

    version 1.0.o

---
+++ models/trips.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const tripSchema = new Schema({
+    createdAt: { type: String, required: true },
+    dateStarted: { type: String, required: true },
+    origin: { type: String, required: true },
+    destination: { type: String, required: true },
+    driver: { type: String, required: true },
+    client: { type: String, required: true },
+    dateEnd: { type: String, required: true },
+})
+
+const model = mongoose.model('Trip', tripSchema);
+module.exports = model;
---
+++ server.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+const mongoose = require('mongoose');
+const Trip = require('./models/trips')
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+const methodOverride = require('method-override');
+
+
+const URL = "mongodb+srv://Nitesh:mayday9501@ecommerceweb.efse8.mongodb.net/PROJECT0?retryWrites=true&w=majority";
+
+
+app.set('view engine', 'ejs');
+app.use(methodOverride("_method"));
+
+app.get('/', async(req, res) => {
+    var data = await Trip.find();
+    console.log(data)
+    res.render('list', { data: data });
+})
+app.post('/', async(req, res) => {
+
+    var obj = {
+        createdAt: req.body.createdAt,
+        origin: req.body.origin,
+        destination: req.body.destination,
+        driver: req.body.driver,
+        client: req.body.client,
+    };
+    console.log("posting...", obj)
+    try {
+        console.log(req.body)
+        var doc = await Trip.create(req.body);
+        console.log('doc created: ', doc);
+        res.redirect('/')
+    } catch (err) {
+        console.log('could not post: ', err.message)
+    }
+})
+app.put('/:id', async(req, res) => {
+    await Trip.findByIdAndUpdate(req.params.id, req.body, (err, docs) => {
+        if (err) {
+            console.log('could not edit: ', err.message)
+        } else {
+            res.redirect('/')
+        }
+    })
+})
+app.delete('/:id', async(req, res) => {
+    await Trip.findByIdAndDelete(req.params.id, (err, docs) => {
+        if (err) {
+            console.log('could not delete: ', err.message)
+        } else {
+            console.log('deleted: ', docs);
+            res.redirect('/')
+        }
+    });
+
+})
+
+app.get('/new', (req, res) => {
+    res.render('new')
+})
+
+app.get('/edit/:id', async(req, res) => {
+    var data = await Trip.findById(req.params.id);
+    res.render('edit', { data: data });
+})
+
+app.get('/delete/:id', async(req, res) => {
+    var data = await Trip.findById(req.params.id);
+    res.render('delete', { data: data });
+})
+
+const connection = async(URL) => {
+    //const URL = 'mongodb+srv://Nitesh:mayday9501@ecommerceweb.efse8.mongodb.net/PROJECT0?retryWrites=true&w=majority'
+    try {
+        await mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+        console.log('DB connected successfully')
+    } catch (err) {
+        console.log('Error: ', err.message);
+    }
+}
+
+connection(process.mongodb_uri || URL);
+
+var PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log('listening to port 3500')
+});
